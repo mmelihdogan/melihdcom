9314e6570859cf37721bba983efcbbd2a8153bf7
Melih <42299017+mmelihdogan@users.noreply.github.com>
2022-07-15 00:23:52 +0300

    creating base draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='border'></div>
+      <div class="col-lg-8 mx-auto p-3 py-md-5">
+        <header class="align-items-center pb-3 mb-5">
+          <h1 className="melih">Me!ih</h1>
+          <h2 className="desc text-muted">fresh graduate; multi • potential • ite</h2>
+        </header>
+
+        <main>
+          <h1></h1>
+          <p class="lead">
+            Hello! I'm Melih Doğan, fresh graduate from Management Information Systems at Boğaziçi University. I'm a strong team player who is able to quickly learn from his fellows and seniors.
+          </p>
+          <p class="lead">My path with coding crossed first when I was a freshman but I did not delve into it back then. Afterwards, I had the opportunity of attending front-end web development bootcamp in 2020. In 5-months-long intensive training focusing on JS and React, I also participated various team and solo projects. Then I took a break for a while but I didn't idle around afterward, of course. While I was exploring the start-up and entrepreneurship ecosystem in Turkey during my project assistant time in Yıldız Technopark, I took courses in various fields such as security, cloud computing, and mobile application development at the college.</p>
+
+        </main>
+        <footer class="pt-5 my-5 text-muted border-top">
+          My personal website is still in the process of design!!!
+        </footer>
+      </div>
     </div>
   );
 }
